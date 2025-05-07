7a8b28c6a10934989ac54807e89b155f4030a2b8
tahoad <nowwatchitclosely@gmail.com>
2025-05-07 23:53:22 +0700

    signalR chat app for learning

---
+++ SignalRChatHub/SignalRChatHub/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatHub.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Clients.All.SendAsync("RecieveMessage",user,message);
+        }
+    }
+}
